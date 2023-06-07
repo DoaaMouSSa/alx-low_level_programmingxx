5befe49e12e9b4910a5454e0157087f6c5f5e049
DoaaMouSSa <doaamoussa5@gmail.com>
2023-06-07 10:10:42 -0700

    task2

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,11 +8,11 @@
 
 void _puts_recursion(char *s)
 {
-	if(*s == '\0')
+	if (*s == '\0')
     		{
-			printf("\n");
-        		return;
+		printf("\n");
+        	return;
     		}
-    	printf("%c", *s);
+    	printf("%c" ,*s);
     	_puts_recursion(++s);
 }
---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ *_print_rev_recursion - a function that prints a string in reverse, followed by a new line
+ * @s: input
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0') {
+        _print_rev_recursion(s + 1);
+        _putchar(*s);
+	}
+}
