43acef57e73849d77bf53cd6fec1e420d9c66d74
DoaaMouSSa <doaamoussa5@gmail.com>
2023-06-09 15:32:40 -0700

    test

---
+++ 0x09-static_libraries/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - a function that print progrm name
+ * @argc: int
+ * @argv: array of strings
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
