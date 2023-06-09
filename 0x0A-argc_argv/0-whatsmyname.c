a3fd3a75108b0307b5f60cc8d2bcc176f1ab5a26
DoaaMouSSa <doaamoussa5@gmail.com>
2023-06-09 09:38:35 -0700

    1-task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
 /**
- *main - a function that print progrm name
+ * main - a function that print progrm name
  * @argc: int
  * @argv: array of strings
- * Return: 
+ * Return: 0
 */
 
 int main(int argc, char *argv[])
 {
-	void(argc);
-	printf("%s\n",argv[argc-1);
-	return 0;
+	printf("%s\n", argv[argc - 1])
+	return (0);
 }
