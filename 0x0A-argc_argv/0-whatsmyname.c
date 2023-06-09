b1bd26a6c6080230856c970abd39e2808335f35c
DoaaMouSSa <doaamoussa5@gmail.com>
2023-06-09 09:29:38 -0700

    1-task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - a function that print progrm name
+ * @argc: int
+ * @argv: array of strings
+ * Return: 
+*/
+
+int main(int argc, char *argv[])
+{
+	void(argc);
+	printf("%s\n",argv[argc-1);
+	return 0;
+}
