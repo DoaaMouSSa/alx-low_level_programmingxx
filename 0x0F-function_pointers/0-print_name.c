9eb17bb9a9f245ba49db8b6e41fc26c8cf541d49
DoaaMouSSa <doaamoussa5@gmail.com>
2023-06-21 13:43:26 -0700

    task1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,10 +4,10 @@
 /**
  * print_name - prints a name
  * @name: name of the person
- *
+ *@f: pointer to a function
  * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {
-    f(name);
+	f(name);
 }
